256e37788fec5d0d310e7cc8203da097984ce082
Ben Huggins <benshuggins@gmail.com>
2019-02-04 10:44:27 -0700

    first commit

---
+++ DeckOfOneCard24/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DeckOfOneCard24
+//
+//  Created by Ben Huggins on 2/4/19.
+//  Copyright © 2019 User. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
